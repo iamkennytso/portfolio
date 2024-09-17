783a8e265ecddbc6a1dd5690220417ba1facdc30
Kenneth Tso <iamkennytso@gmail.com>
2024-09-17 00:48:52 -0400

    meta data update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./ui/nav";
+import Head from 'next/head';
 
 const defaultFont = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kenneth Tso Portfolio Site",
+  title: "Kenneth Tso - Software Engineer",
   description: "Kenneth Tso Software Engineer Website",
 };
 
@@ -18,6 +19,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <title>Kenneth Tso - Software Engineer</title>
+        <meta name="description" content="Kenneth Tso's Portfolio Website" />
+      </Head>
       <body className={defaultFont.className}>
         <Nav />
         {children}
