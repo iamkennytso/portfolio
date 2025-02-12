748a69140f16f31949c5475510067ea000a6a7c1
Kenneth Tso <iamkennytso@gmail.com>
2025-02-11 20:34:37 -0500

    typo

--- src/app/wonderlic/page.tsx
+++ src/app/wonderlic/page.tsx
@@ -38,7 +38,7 @@ const wonderlicImages: WonderlicObject[] = [
 export default function CoverLetter() {
   return (
     <div className="flex flex-col m-16 justify-center items-center">
-      <p>What is Wonderlic? According to their <a className="underline" href={wonderlicUrl}>website</a>, &quot;The Wonderlic Select hiring assessments provide comprehensive, job-specific insights into a candidate&apos;s fit for a role. Using Wonderlic&apos;s multi-measure assessment approach gives HR teams the most accurate prediction of whether their candidate can do the job, wants to do the job, and how they&apos;ll do it. This approach gives greater insights into performance and retention.`&quot;`</p>
+      <p>What is Wonderlic? According to their <a className="underline" href={wonderlicUrl}>website</a>, &quot;The Wonderlic Select hiring assessments provide comprehensive, job-specific insights into a candidate&apos;s fit for a role. Using Wonderlic&apos;s multi-measure assessment approach gives HR teams the most accurate prediction of whether their candidate can do the job, wants to do the job, and how they&apos;ll do it. This approach gives greater insights into performance and retention.&quot;</p>
       <br/>
       <p> I took this personality test as part of a job application. I was actually rejected from the job, so I&apos;m not sure putting this out there actually helps me, but here it is for transparency sake.</p>
       {wonderlicImages.map(({url, alt, width, height}) => <Image key={url} width={width} height={height} className="m-4" src={url} alt={alt} /> )}
