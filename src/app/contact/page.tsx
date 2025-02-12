1e2d5c9adcae344bcb8515d5e6f667518cda31a5
Kenneth Tso <iamkennytso@gmail.com>
2025-02-11 20:23:57 -0500

    lint

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,9 @@
 export default function CoverLetter() {
   return (
     <div className='mt-16 align-center justify-center'>
-      <p className='text-center'>The best way to reach Ken via email at: <a href="mailto:iamkennytso@gmail.com">iamkennytso@gmail.com</a></p><br/>
-      <p className='text-center'><a href="https://www.linkedin.com/in/iamkennytso" target="_blank">LinkedIn</a></p><br/>
-      <p className='text-center'><a href="https://github.com/iamkennytso" target="_blank">Github</a></p><br/>
+      <p className='text-center'>The best way to reach Ken via email at: <a className='underline' href="mailto:iamkennytso@gmail.com">iamkennytso@gmail.com</a></p><br/>
+      <p className='text-center'><a className='underline' href="https://www.linkedin.com/in/iamkennytso" target="_blank">LinkedIn</a></p><br/>
+      <p className='text-center'><a className='underline' href="https://github.com/iamkennytso" target="_blank">Github</a></p><br/>
     </div>
   );
 }
--- src/app/wonderlic/page.tsx
+++ src/app/wonderlic/page.tsx
@@ -1,19 +1,20 @@
-import { wonderlic1, wonderlic2, wonderlic3, wonderlic4 } from "../../consts/urls";
+import Image from "next/image";
+import { wonderlicUrl, wonderlic1, wonderlic2, wonderlic3, wonderlic4 } from "../../consts/urls";
 
 const imageToAltMap: Record<string, string> = {
-  [wonderlic1]: 'Highly Spontaneous, Driven by Persuasive Work, Strong Problem-Solver', 
-  [wonderlic2]: 'Highly Spontaneous, Highly Outgoing, Moderately Conventional, Moderately Candid, Moderately Composed', 
-  [wonderlic3]: 'Driven by Persuasive Work, Interested in Hands-On Work, Interested in Service Work, Indifferent to Creative Work, Indifferent to Analytical Work, Uninterested in Clerical Work',
-  [wonderlic4]: 'Strong Problem-Solver, Highly Independent, Indifferent to Leadership'
+  [wonderlic1]: "Highly Spontaneous, Driven by Persuasive Work, Strong Problem-Solver", 
+  [wonderlic2]: "Highly Spontaneous, Highly Outgoing, Moderately Conventional, Moderately Candid, Moderately Composed", 
+  [wonderlic3]: "Driven by Persuasive Work, Interested in Hands-On Work, Interested in Service Work, Indifferent to Creative Work, Indifferent to Analytical Work, Uninterested in Clerical Work",
+  [wonderlic4]: "Strong Problem-Solver, Highly Independent, Indifferent to Leadership"
 }
 
 export default function CoverLetter() {
   return (
     <div className="flex flex-col m-16 justify-center align-center">
-      <p>What is Wonderlic? According to their website, The Wonderlic Select hiring assessments provide comprehensive, job-specific insights into a candidate's fit for a role. Using Wonderlic's multi-measure assessment approach gives HR teams the most accurate prediction of whether their candidate can do the job, wants to do the job, and how they'll do it. This approach gives greater insights into performance and retention.</p>
+      <p>What is Wonderlic? According to their <a className="underline" href={wonderlicUrl}>website</a>, &quot;The Wonderlic Select hiring assessments provide comprehensive, job-specific insights into a candidate&apos;s fit for a role. Using Wonderlic&apos;s multi-measure assessment approach gives HR teams the most accurate prediction of whether their candidate can do the job, wants to do the job, and how they&apos;ll do it. This approach gives greater insights into performance and retention.`&quot;`</p>
       <br/>
-      <p> I took this personality test as part of a job application. I was actually rejected from the job, so I'm not sure putting this out there actually helps me, but here it is for transparency sake.</p>
-      {Object.keys(imageToAltMap).map(url => <img className="m-4" src={url} alt={imageToAltMap[url]} /> )}
+      <p> I took this personality test as part of a job application. I was actually rejected from the job, so I&apos;m not sure putting this out there actually helps me, but here it is for transparency sake.</p>
+      {Object.keys(imageToAltMap).map(url => <Image key={url} className="m-4" src={url} alt={imageToAltMap[url]} /> )}
     </div>
   );
 }
