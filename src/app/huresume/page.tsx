2681a520503bd8bd28d56e999975020edb1b1cc3
Ken <iamkennytso@gmail.com>
2026-05-31 08:39:43 -0400

    fix paragraph

--- src/app/huresume/page.tsx
+++ src/app/huresume/page.tsx
@@ -1,10 +1,5 @@
-import PdfContainer from "../ui/pdfContainer"
-
 export default function Huresume() {
   return (
-    Due to privacy concerns, this resume has been taken down. Please email me for my latest resume :)
-    // <PdfContainer>
-      // <iframe src="/Resume_Kenneth_Tso.pdf" height='100%' width='100%'/>
-    {/*</PdfContainer>*/}
+    <p>Due to privacy concerns, this resume has been taken down. Please email me for my latest resume :)</p>
   );
 }
--- src/app/paresume/page.tsx
+++ src/app/paresume/page.tsx
@@ -1,10 +1,5 @@
-import PdfContainer from "../ui/pdfContainer"
-
 export default function Paresume() {
   return (
-    Due to privacy concerns, this resume has been taken down. Please email me for my latest resume :)
-    // <PdfContainer>
-    //   <iframe src="/Resume_Kenneth_Tso_LLM.pdf" height='100%' width='100%'/>
-    // </PdfContainer>
+    <p>Due to privacy concerns, this resume has been taken down. Please email me for my latest resume :)</p>
   );
 }
